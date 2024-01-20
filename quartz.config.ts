6a205393b076722634380281c79e3c97af85fce5
Giovyx-90 <giovyx90.principale@gmail.com>
2024-01-20 13:56:57 +0100

    Quartz sync: Jan 20, 2024, 1:56 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "giovyx90.net/home",
+    baseUrl: "giovyx90.net",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
